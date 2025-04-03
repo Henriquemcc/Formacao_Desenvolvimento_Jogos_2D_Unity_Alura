6622420209726d16b3882dde120dbce9d105c9fe
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-04-03 14:15:07 -0300

    Implementando a impressão de mensagem de debug ao apertar espaço para pular. (Parte 5 da aula 2 do curso 'Unity: criando um jogo metroidvania 2D')

--- A-Vinganca-De-Blaid-2D/Assets/Scripts/Player/PlayerBehaviour.cs
+++ A-Vinganca-De-Blaid-2D/Assets/Scripts/Player/PlayerBehaviour.cs
@@ -4,9 +4,18 @@ public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5;
 
+    private void Start()
+    {
+        GameManager.Instance.inputManager.OnJump += HandleJump;
+    }
+
     private void Update()
     {
         float moveDirection = GameManager.Instance.inputManager.Movement * Time.deltaTime * moveSpeed;
         transform.Translate(moveDirection, 0, 0);
+    }
+
+    private void HandleJump() {
+        Debug.Log("Estou pulando!");
     }
 }
