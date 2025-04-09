2fd5d74f0dac5a7351d09cb357572556751cbb14
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-04-09 17:49:18 -0300

    Implementando alteração do parâmetro 'isJumping' do Animator, caso o personagem esteja pulando. (Parte 10 da aula 3 do curso 'Unity: criando um jogo metroidvania 2D')

--- A-Vinganca-De-Blaid-2D/Assets/Scripts/Player/PlayerAnim.cs
+++ A-Vinganca-De-Blaid-2D/Assets/Scripts/Player/PlayerAnim.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 public class PlayerAnim : MonoBehaviour
 {
     private Animator animator;
+    private IsGroundedChecker isGroundedChecker;
 
     void Awake()
     {
         animator = GetComponent<Animator>();
+        isGroundedChecker = GetComponent<IsGroundedChecker>();
     }
 
     void Update()
     {
         bool isMoving = GameManager.Instance.inputManager.Movement != 0;
         animator.SetBool("isMoving", isMoving);
+        bool isJumping = !isGroundedChecker.IsGrounded();
+        animator.SetBool("isJumping", isJumping);
     }
 }
