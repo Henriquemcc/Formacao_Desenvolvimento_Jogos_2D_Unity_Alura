6220d43af316040b0947f9d6c67aaa40344edaba
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-04-21 17:34:17 -0300

    Alterando o script do inimigo corporal para que ele ataque quando ele puder atacar. (Parte 4 da aula 4 do curso 'Unity: criando um jogo metroidvania 2D')

--- A-Vinganca-De-Blaid-2D/Assets/Scripts/Enemy/MeleeEnemy.cs
+++ A-Vinganca-De-Blaid-2D/Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -9,7 +9,15 @@ public class MeleeEnemy : BaseEnemy
 
     protected override void Update()
     {
-        print("is in sight? " + PlayerInSight());
+        VerifyCanAttack();
+    }
+
+    private void VerifyCanAttack()
+    {
+        if (PlayerInSight())
+        {
+            animator.SetTrigger("attack");
+        }
     }
 
     private bool PlayerInSight()
