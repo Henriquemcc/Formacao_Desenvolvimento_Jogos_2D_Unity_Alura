d18d9adaf4ee2bbc1636be71891e025ae229a57e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-04-21 16:50:17 -0300

    Adicionando script que define o comportamento de inimigos corpo a corpo. (Parte 3 da aula 4 do curso 'Unity: criando um jogo metroidvania 2D')

---
+++ A-Vinganca-De-Blaid-2D/Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -0,0 +1,29 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MeleeEnemy : BaseEnemy
+{
+    [SerializeField] private Transform detectPosition;
+    [SerializeField] private Vector2 detectBoxSize;
+    [SerializeField] private LayerMask playerLayer;
+
+    protected override void Update()
+    {
+        print("is in sight? " + PlayerInSight());
+    }
+
+    private bool PlayerInSight()
+    {
+        Collider2D playerCollider = Physics2D.OverlapBox(detectPosition.position, detectBoxSize, 0f, playerLayer);
+        return playerCollider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (detectPosition != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(detectPosition.position, detectBoxSize); 
+        }
+    }
+}
